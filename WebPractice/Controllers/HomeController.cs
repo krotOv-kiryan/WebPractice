d6c9610fad4100f6b535d99d10113867382731ae
krotOv-kiryan <kirya.krotov.03@mail.ru>
2021-11-16 20:12:32 +1000

    s2

--- WebPractice/Controllers/HomeController.cs
+++ WebPractice/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace WebPractice.Controllers
             return View();
         }
 
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
